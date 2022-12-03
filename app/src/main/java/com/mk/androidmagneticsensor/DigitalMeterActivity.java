7791920b40a292c03d5feae2bad1ee6aff18d431
Mustafa Kamal <35607112+MK-soft-maker@users.noreply.github.com>
2022-12-03 07:17:39 +0500

    Adding two Buttons Adding to Activities

---
+++ app/src/main/java/com/mk/androidmagneticsensor/DigitalMeterActivity.java
@@ -0,0 +1,14 @@
+package com.mk.androidmagneticsensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DigitalMeterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digital_meter);
+    }
+}
